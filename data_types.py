eb09a10b5fc82766badf764a2c3b4e5562f36ada
Bygabite <rjwjr1031@gmail.com>
2024-08-05 22:52:00 -0400

    getting better at committing

---
+++ data_types.py
@@ -0,0 +1,32 @@
+#always comment your code
+var = "test"
+
+print(type(var))
+
+var2 = 493
+
+print(type(var2))
+
+var3 = '649'
+
+print(type(var3))
+
+var4 = "can't"
+var5 = ' "Get rich and Live in abundance" '
+var6 = '"can\'t means won\'t"'
+ 
+print(var4)
+print(var5)
+print(var6)
+
+var7 = int(var3)
+
+print(type(var7))
+
+var8 = "We need a line break here.\nOkay that's good"
+
+print(var8)
+
+var9 = ''' We used a multi line string here.
+okay thats good.'''
+#Always comment your code
