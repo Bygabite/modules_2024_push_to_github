8e1c00ebf11f5f2bf12e849dd86e70ba34b420cd
Bygabite <rjwjr1031@gmail.com>
2024-08-07 09:50:06 -0400

    Create module5_return.py

---
+++ module5_return.py
@@ -0,0 +1,38 @@
+def get_average(input_numbers):
+    sum = 0.0
+    for number in input_numbers:
+        sum += number
+    average = sum / len(input_numbers)
+    return average
+
+print('The average is:', get_average([5.0, 3.5, 7.8, 9.9, 10.0]))
+
+average = get_average([5.0, 3.5, 7.8, 9.9, 10.0])
+if average is > 5.0:
+    print('The average is too high!')
+
+
+get_average([2])
+
+
+def is_first_last_equal(number_list):
+    if (number_list[0] == number_list[-1]):
+        return True
+    else:
+        return False
+    
+# an empty list will bring back an error message
+
+def is_first_last_equal(number_list):
+    if len(number_list) == 0:
+        return
+    if (number_list[0] == number_list[-1]):
+        return True
+    else:
+        return False
+    
+    
+
+
+
+
