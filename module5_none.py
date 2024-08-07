b4b210c16241803e1de2e16b1691150030cf57f1
Bygabite <rjwjr1031@gmail.com>
2024-08-07 08:19:02 -0400

    Create module5_none.py

---
+++ module5_none.py
@@ -0,0 +1,31 @@
+#functions 1.cause some effect 2.Return a meaningful value
+print('hello')
+
+length = len('hello')
+
+number = input('what is the number?')
+
+print_return = print('Hello world')
+print(print_return)
+
+x = None
+
+if :
+    print("None is True")
+elif x is False:
+    print ("None is False")
+else:
+    print("None is not True, or False, None is just None")
+
+x = None
+if x = None:
+    print('yes')
+if x == None:
+    print('it does')
+
+def greet():
+    print('hello')
+
+x = greet()
+print(x)
+
